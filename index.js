c3c529dc0018152ec7a1286c721e38a7d018d7e7
GuitherezDika <guitherez.git@gmail.com>
2024-05-26 19:29:23 +0700

    install dotenv and config env

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express(); 
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.lkrdgkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
