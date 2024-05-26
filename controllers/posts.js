40cae8e240f7da7c28950759a9a3d43fdb46a6e2
GuitherezDika <guitherez.git@gmail.com>
2024-05-26 11:46:12 +0700

    create api delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,9 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find(); // find function takes time => await
-        console.log(postMessages);
-        // res.send('Ok!!22')
         res.status(200).json(postMessages)
     } catch (error) {
         res.status(404).json({message: error.message})
@@ -12,13 +11,33 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    // res.send("Post Creation");
     const post = req.body;
     const newPost = new PostMessage(post);
+    // PostMessage = model
     try {
         await newPost.save(); // takes time => await
         res.status(201).json(newPost);
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePost = async(req, res) => {
+    const { id: _id } = req.params;
+    // _id sebagai alias dan dipakai pada code di bawah ini
+    const post = req.body; // client
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id: _id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(400).send('No post with that id');
+
+    // await PostMessage.findByIdAndRemove(_id);
+    await PostMessage.findByIdAndDelete(_id);
+
+    res.json({message: "Post deleted successfully!"});
 }
