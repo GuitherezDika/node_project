ac42b1fae9be9d41cd2e0a94faf07ea0e26ded90
GuitherezDika <guitherez.git@gmail.com>
2024-06-12 11:36:52 +0700

    create memory api

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,13 +6,14 @@ export const getPosts = async (req, res) => {
         const postMessages = await PostMessage.find(); // find function takes time => await
         res.status(200).json(postMessages)
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(404).json({ message: error.message })
     }
 }
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage(post);
+    // setelah client set API Interceptor
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     // PostMessage = model
     try {
         await newPost.save(); // takes time => await
@@ -22,30 +23,30 @@ export const createPost = async (req, res) => {
     }
 }
 
-export const updatePost = async(req, res) => {
+export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     // _id sebagai alias dan dipakai pada code di bawah ini
     const post = req.body; // client
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
     res.json(updatedPost)
 }
 
 export const deletePost = async (req, res) => {
     const { id: _id } = req.params;
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(400).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(400).send('No post with that id');
 
     // await PostMessage.findByIdAndRemove(_id);
     await PostMessage.findByIdAndDelete(_id);
 
-    res.json({message: "Post deleted successfully!"});
+    res.json({ message: "Post deleted successfully!" });
 }
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
-    if(!req.userId) return res.json({ message: "Unauthenticated"})
-    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send('No post with that id');
+    if (!req.userId) return res.json({ message: "Unauthenticated" })
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).send('No post with that id');
     const post = await PostMessage.findById(id);
     const index = post.likes.findIndex(id => id === String(req.userId));
     if (index == -1) {
@@ -53,8 +54,7 @@ export const likePost = async (req, res) => {
     } else {
         post.likes = post.likes.filter(id => id !== String(req.userId))
     }
-    console.log(post);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {new: true});
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     // const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new: true});
 
     res.json(updatedPost)
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ import jwt, { decode } from "jsonwebtoken";
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
+        // set headers auth pada Interceptor API client
         // cek token yang tercreate saat proses api registration 
         const isCustomAuth = token.length < 500;
         // < 500 == token dicreate oleh api regist
